a7239d9f8c435e95c3fb691b49f53cce49a75165
nubialyaktoled <nubia.lyak.martins@gmail.com>
2023-04-24 18:34:04 -0300

    add security

---
+++ api/src/seguranca/seguranca.go
@@ -0,0 +1,17 @@
+package seguranca
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+// Hash recebe uma string e coloca um hash nela
+func Hash(senha string) ([]byte, error) {
+	//gera hash a partir da senha
+	//segundo parametro é o custo da operação, quanto maior, mais dificil, a gente passa o padrão DefaultCost
+	return bcrypt.GenerateFromPassword([]byte(senha), bcrypt.DefaultCost)
+}
+
+// VerificarSenha compara uma senha e um hash e retorna se elas são iguais
+func VerificarSenha(senhaComHash, senhaString string) error {
+	return bcrypt.CompareHashAndPassword([]byte(senhaComHash), []byte(senhaString))
+}
